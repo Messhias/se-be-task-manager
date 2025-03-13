14845bb4a402b55beb97e951f08b742563841c49
Fabio William Conceição <messhias@gmail.com>
2025-03-13 22:11:18 +0000

    Enhance configuration in render.yaml and app.module.ts by adding health check path, retry settings, and logging options. Update main.ts to dynamically set the application port.

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,8 @@ async function bootstrap() {
     customfavIcon: 'https://nestjs.com/img/favicon.ico',
   });
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
